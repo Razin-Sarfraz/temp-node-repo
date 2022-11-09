e354d7140c12d63c999915968efa7d2a8dde7883
razin.sarfraz <razin.sarfraz@rnssol.com>
2022-11-09 10:44:04 +0500

    9th commit

--- 12-http.js
+++ 12-http.js
@@ -3,18 +3,19 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-   ;
+    return res.end("Welcome to Home Page!");
     // res.end();
   }
   if (req.url === "/about") {
     return res.end("Welcome to About Page!");
     //res.end();
   }
-  return res.end(`<h1>Bad Request</h1><p>Page not found</p><a href="/">Home Page</a>`);
+  return res.end(
+    `<h1>Bad Request</h1><p>Page not found</p><a href="/">Home Page</a>`
+  );
 });
 
 server.listen(5000);
-
 
 // const http = require("http");
 // const server = http.createServer((req, res) => {
---
+++ 14-request-event.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+//   });
+
+//using emitter API
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  res.end("Welcome");
+});
+
+server.listen(5000);
